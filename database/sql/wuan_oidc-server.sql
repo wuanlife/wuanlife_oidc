572892fa0c58fb3d787c80e7d6bd3a76c3f13910
Gtaker <459472218@qq.com>
2018-03-11 23:26:51 +0800

    database structure

---
+++ database/sql/wuan_oidc-server.sql
@@ -0,0 +1,72 @@
+-- 用户基础表
+CREATE TABLE IF NOT EXISTS users_base
+(
+  id        INT UNSIGNED AUTO_INCREMENT  NOT NULL
+  COMMENT '用户id',
+  mail      VARCHAR(30) COLLATE utf8_bin NOT NULL
+  COMMENT '用户邮箱',
+  name      CHAR(20) COLLATE utf8_bin    NOT NULL
+  COMMENT '用户名',
+  password  CHAR(32) COLLATE utf8_bin    NOT NULL
+  COMMENT '用户密码',
+  create_at TIMESTAMP                    NOT NULL
+  COMMENT '注册时间',
+  PRIMARY KEY (id),
+  KEY login_index(mail, password)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_bin
+  COMMENT ='用户基础信息表';
+
+-- 用户头像表
+CREATE TABLE IF NOT EXISTS avatar_url
+(
+  user_id    INT UNSIGNED AUTO_INCREMENT   NOT NULL
+  COMMENT '用户id',
+  url        VARCHAR(255) COLLATE utf8_bin NOT NULL
+  COMMENT '图片url',
+  delete_flg TINYINT UNSIGNED              NOT NULL DEFAULT 0
+  COMMENT '图片是否已删除',
+  KEY image_user_id(user_id)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_bin
+  COMMENT ='用户头像url表';
+
+-- 用户权限表
+-- 用于实现管理员功能，
+-- 因为取消了group功能，所以身份关系是唯一的，但是考虑到日后的可扩展性，
+-- 此处使用 | 操作符来进行状态的叠加，使用 & 运算符进行状态的判断
+CREATE TABLE IF NOT EXISTS users_auth
+(
+  id   INT UNSIGNED              NOT NULL
+  COMMENT '用户id',
+  name CHAR(20) COLLATE utf8_bin NOT NULL
+  COMMENT '用户名',
+  auth INT UNSIGNED              NOT NULL
+  COMMENT '用户权限',
+  PRIMARY KEY (id),
+  KEY auth(auth)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_bin
+  COMMENT ='用户权限表';
+
+-- 权限对应关系表
+-- 使用 << 运算符来得到不同的状态码
+CREATE TABLE IF NOT EXISTS auth_detail
+(
+  id        INT UNSIGNED AUTO_INCREMENT  NOT NULL
+  COMMENT '权限id(偏移量)',
+  indentity VARCHAR(30) COLLATE utf8_bin NOT NULL
+  COMMENT '权限类型',
+  PRIMARY KEY (id),
+  UNIQUE KEY (indentity)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_bin
+  COMMENT ='权限对应关系表';
