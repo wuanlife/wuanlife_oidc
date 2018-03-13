d23c4b2b798bbb82db0d6757e85a027e2f3a4d1e
Gtaker <459472218@qq.com>
2018-03-13 22:19:11 +0800

    权限验证中间件，在每次请求前验证是否存在ID-Token

---
+++ app/Http/Middleware/AuthMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\JwtVerifier;
+
+class AuthMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        try {
+            // 检测 ID-Token 是否存在
+            $id_token = $request->header(AuthController::ID_TOKEN_KEY);
+            if (!$id_token) {
+                return \response(['error' => '缺少ID-Token'], 400);
+            }
+            // 检测 ID-Token 合法性
+            JwtVerifier::verifyToken($id_token, 'ID');
+        } catch (\Exception $JWTException) {
+            return \response(['error' => $JWTException->getMessage()], 400);
+        }
+
+        return $next($request);
+    }
+}
