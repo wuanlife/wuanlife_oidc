19522b8a334d42073d07cde388ee2fc845e2f7a3
Gtaker <459472218@qq.com>
2018-05-14 16:59:25 +0800

    为路由添加正则验证

--- routes/api.php
+++ routes/api.php
@@ -21,9 +21,9 @@ Route::group([
     'middleware' => [
         'check_id_token',
     ],
-], function (){
+], function () {
     // A2 申请授权接口
-    Route::post('/auth','AuthController@getAccessToken');
+    Route::post('/auth', 'AuthController@getAccessToken');
 });
 
 
@@ -32,26 +32,25 @@ Route::group([
         'check_id_token',
         'check_access_token',
     ]
-],function () {
+], function () {
     // U3 获取用户信息接口
-    Route::get('/users/{id}','UsersController@getUserInfo');
+    Route::get('/users/{id}', 'UsersController@getUserInfo')->where('id', '[0-9]+');
     // U5 修改用户信息接口
-    Route::put('/users/{id}','UsersController@editorUserInfo');
+    Route::put('/users/{id}', 'UsersController@editorUserInfo')->where('id', '[0-9]+');
 });
 
 
 Route::group([
 
-], function (){
+], function () {
     // A1 验证Token完整性接口
-    Route::get('/auth','AuthController@verifyToken');
+    Route::get('/auth', 'AuthController@verifyToken');
     // U1 登录接口
-    Route::post('/users/login','UsersController@login');
+    Route::post('/users/login', 'UsersController@login');
     // U2 注册接口
-    Route::post('/users/register','UsersController@register');
+    Route::post('/users/register', 'UsersController@register');
     // U4 退出登录接口
-    Route::post('/users/logout','UsersController@logout');
+    Route::post('/users/logout', 'UsersController@logout');
     // U6 对午安应用返回用户信息接口
-    Route::get('/app/users/{id}','UsersController@responseUserInfoToApp');
+    Route::get('/app/users/{id}', 'UsersController@responseUserInfoToApp')->where('id', '[0-9]+');
 });
-
