c466531b7f4c18989d67aeb142906504cfe6d283
Gtaker <459472218@qq.com>
2018-05-21 18:47:38 +0800

    为 reset_password 表增加 created_at 字段

--- database/sql/v1.0.2.sql
+++ database/sql/v1.0.2.sql
@@ -110,10 +110,11 @@ CREATE TABLE IF NOT EXISTS auth_detail
 -- 找回密码表
 CREATE TABLE IF NOT EXISTS reset_password
 (
-  user_id INT UNSIGNED NOT NULL
+  user_id    INT UNSIGNED                        NOT NULL
   COMMENT '用户id',
-  token   VARCHAR(255) COLLATE utf8_bin COMMENT '验证token',
-  exp     TIMESTAMP   default CURRENT_TIMESTAMP NOT NULL
+  token      VARCHAR(255) COLLATE utf8_bin COMMENT '验证token',
+  created_at TIMESTAMP default CURRENT_TIMESTAMP NOT NULL,
+  exp        TIMESTAMP default CURRENT_TIMESTAMP NOT NULL
   COMMENT '过期时间',
   PRIMARY KEY (user_id)
 )
@@ -127,7 +128,7 @@ CREATE TABLE IF NOT EXISTS wuan_points
 (
   user_id INT UNSIGNED NOT NULL
   COMMENT '用户id',
-  points INT UNSIGNED NOT NULL DEFAULT 0
+  points  INT UNSIGNED NOT NULL DEFAULT 0
   COMMENT '午安积分',
   PRIMARY KEY (user_id)
 )
@@ -136,14 +137,14 @@ CREATE TABLE IF NOT EXISTS wuan_points
   COLLATE = utf8_bin
   COMMENT = '午安积分表';
 
-  -- 积分兑换记录表
-CREATE TABLE points_order
+-- 积分兑换记录表
+CREATE TABLE IF NOT EXISTS points_order
 (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  user_id INT UNSIGNED NOT NULL
+  id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  user_id      INT UNSIGNED NOT NULL
   COMMENT '用户id',
-  points_alert  INT NOT NULL
+  points_alert INT          NOT NULL
   COMMENT '午安影视积分',
-  created_at TIMESTAMP,
+  created_at   TIMESTAMP,
   PRIMARY KEY (id)
 )
