5d8e1d02b900ccf311ced40f8449802cc6660094
AilonHuang <529678737@qq.com>
2018-09-22 19:46:58 +0800

    修改 性别女的英文

--- database/sql/v1.0.1-insert.sql
+++ database/sql/v1.0.1-insert.sql
@@ -1,5 +1,5 @@
 -- 添加性别对应关系
-INSERT INTO sex_detail VALUES ('male','男'), ('famale','女'), ('secrecy','保密');
+INSERT INTO sex_detail VALUES ('male','男'), ('female','女'), ('secrecy','保密');
 
 -- 添加管理员对应关系
 INSERT INTO auth_detail VALUES (1, '超级管理员'), (2, '管理员');
